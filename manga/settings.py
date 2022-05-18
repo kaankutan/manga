cf6eeca25caf39de811fb34c27c9f9e004d18985
Mustafa Kaan Kutan <59696178+kaankutan@users.noreply.github.com>
2022-05-18 18:43:50 +0300

    Initial commit

--- manga/settings.py
+++ manga/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(e9fq5y7(^zs=m+urps)ug3b^xgt)u_@1abl3+3x@f=lfn5r!l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
